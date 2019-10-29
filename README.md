d6c0e3132fefab64a871a8714e9094206677d55a
Brendan Asselstine <mail.asselstine@gmail.com>
2019-10-28 18:19:34 -0700

    Initial comit

---
+++ README.md
@@ -0,0 +1,20 @@
+# OpenZeppelin Ethers
+
+Provides an Ethers.js console to interact with your contracts.
+
+## Usage
+
+Usage: oze [options] [command]
+
+Options:
+  -n, --network <network>  selects network.  "localhost" will be interpreted as http://localhost:8545 (default: "mainnet")
+  -h, --help               shows this help
+
+Commands:
+  console                  Launches a repl console
+  
+  Includes global variables:
+  
+  contracts: Every project contract discovered, including ProxyAdmin
+  provider: an ethers provider
+  ethers: the ethers lib
---
+++ index.js
@@ -0,0 +1,101 @@
+#!/usr/bin/env node
+const program = require('commander')
+const chalk = require('chalk')
+const repl = require('repl')
+const stubber = require('async-repl/stubber');
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+
+
+
+program
+  .option('-n, --network <network>', 'selects network.  "localhost" will be interpreted as http://localhost:8545', 'mainnet')
+  .helpOption('-h, --help', 'shows this help')
+
+program
+  .command('console')
+  .description(
+    `Launches a node repl
+
+Includes global variables:
+
+contracts: Every project contract discovered, including ProxyAdmin
+provider: an ethers provider
+ethers: the ethers lib
+`
+  )
+  .action(function (env, options) {
+    
+    global.logp = function (promise) {
+      promise.then(console.log).catch(console.error)
+    }
+    
+    global.ethers = require('ethers')
+    
+    const ProxyAdminJson = require('@openzeppelin/upgrades/build/contracts/ProxyAdmin.json')
+    
+    global.contracts = {
+      ProxyAdmin: ProxyAdminJson.abi
+    }
+    
+    let projectFile = JSON.parse(fs.readFileSync('./.openzeppelin/project.json'))
+
+    let ozContractFile
+    if (program.network === 'localhost') {
+      global.provider = new ethers.providers.JsonRpcProvider('http://localhost:8545')
+      const matches = glob.sync('./openzeppelin/dev-*.json')
+      if (matches.length) {
+        ozContractFile = matches[matches.length - 1]
+      } else {
+        console.error(chalk.red('Cannot find contract file for localhost'))
+      }
+    } else {
+      global.provider = new ethers.getDefaultProvider(program.network)
+      ozContractFile = `./.openzeppelin/${program.network}.json`
+    }
+
+    console.log(chalk.green(`Using ${ozContractFile}...`))
+    let ozContracts = JSON.parse(fs.readFileSync(ozContractFile))
+
+    const artifactsDir = './build/contracts'
+    const artifactNames = fs.readdirSync(artifactsDir)
+    artifactNames.map(artifactName => {
+      const artifact = JSON.parse(fs.readFileSync(path.join(artifactsDir, artifactName)))
+      global.contracts[artifact.contractName] = {
+        abi: artifact.abi,
+        bytecode: artifact.bytecode
+      }
+
+      const proxyName = `${projectFile.name}/${artifact.contractName}`
+
+      if (ozContracts.proxies[proxyName]) {
+        const proxies = ozContracts.proxies[proxyName]
+        const lastProxy = proxies[proxies.length - 1]
+        global.contracts[artifact.contractName].address = lastProxy.address
+        global[artifact.contractName] = new ethers.Contract(lastProxy.address, artifact.abi, provider)
+      }
+    })
+    
+    const instance = repl.start({
+      useGlobal: true,
+      prompt: 'oze> '
+    })    
+
+    stubber(instance);
+  })
+
+// when bad command
+program
+  .on('command:*', function(){
+    program.help()
+  });
+
+program.parse(process.argv)
+
+// error on unknown commands
+var NO_COMMAND_SPECIFIED = program.args.length === 0;
+
+if (NO_COMMAND_SPECIFIED) {
+  program.help()
+}
